4ef873f0d5d617003c7ba05a71cbe53993070bf0
Nick Cromwell <cromwellnick@gmail.com>
2019-07-08 09:36:34 -0400

    PasswordHash renamed to Password

--- src/Ranger.Services.Tenants/Messages/User.cs
+++ src/Ranger.Services.Tenants/Messages/User.cs
@@ -1,15 +1,15 @@
 namespace Ranger.Services.Tenants {
     public class User {
-        public User (string email, string firstName, string lastName, string passwordHash) {
+        public User (string email, string firstName, string lastName, string password) {
             this.Email = email;
             this.FirstName = firstName;
             this.LastName = lastName;
-            this.PasswordHash = passwordHash;
+            this.Password = password;
 
         }
         public string Email { get; }
         public string FirstName { get; }
         public string LastName { get; }
-        public string PasswordHash { get; }
+        public string Password { get; }
     }
 }
