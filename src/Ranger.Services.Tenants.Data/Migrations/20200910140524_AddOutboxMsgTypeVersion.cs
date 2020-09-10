2bbefe8daa4fe9723810f2035163667f97f1fcc3
Nick Cromwell <cromwellnick@gmail.com>
2020-09-10 10:08:22 -0400

    Add Message Type and Version columns

---
+++ src/Ranger.Services.Tenants.Data/Migrations/20200910140524_AddOutboxMsgTypeVersion.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ranger.Services.Tenants.Data.Migrations
+{
+    public partial class AddOutboxMsgTypeVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "message_version",
+                table: "ranger_rabbit_message",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<string>(
+                name: "type",
+                table: "ranger_rabbit_message",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "message_version",
+                table: "ranger_rabbit_message");
+
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "ranger_rabbit_message");
+        }
+    }
+}
