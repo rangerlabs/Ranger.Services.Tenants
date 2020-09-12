e713205b2ab16a80af25e9b1bbdb9a3718a74ca4
Nick Cromwell <cromwellnick@gmail.com>
2020-09-12 13:41:34 -0400

    Enforce Outbox one to one relationship

---
+++ src/Ranger.Services.Tenants.Data/Migrations/20200912174050_OutboxOneToOneRelationship.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ranger.Services.Tenants.Data.Migrations
+{
+    public partial class OutboxOneToOneRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_outbox_message_id",
+                table: "outbox");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_outbox_message_id",
+                table: "outbox",
+                column: "message_id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_outbox_message_id",
+                table: "outbox");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_outbox_message_id",
+                table: "outbox",
+                column: "message_id");
+        }
+    }
+}
